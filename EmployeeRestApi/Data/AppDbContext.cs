ef4568d3e6be655d517caf439e6b85ef41e636ab
gilbertkibet <kibetgilly354@gmail.com>
2022-12-12 16:33:52 +0300

    Added Db Context Class and Connections String ,And Do migrations to create new Table

---
+++ EmployeeRestApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeRestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeRestApi.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options ) : base( options ) 
+        {
+
+        }
+
+        public DbSet<Employee> EmployeeTable { get; set; }
+    }
+}
